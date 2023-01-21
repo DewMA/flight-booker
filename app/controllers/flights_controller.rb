d40230e42d119b47869702e2bcd4cbf7f0170861
Nathaniel Simon <95974868+deLaVerendrye@users.noreply.github.com>
2023-01-21 17:04:51 +0800

    New Commit

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,16 @@
 class FlightsController < ApplicationController
 
     def index
-        @results = Flight.all
+        from = Airport.find_by(name: get_params[:from])
+        to = Airport.find_by(name: get_params[:to])
+
+        @results = Flight.find_by(from_id: from.id)
+    end
+
+    private
+
+    def get_params
+        params[:flight]
     end
 
 end
