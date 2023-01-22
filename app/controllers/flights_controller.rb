99f83a5a70893ad657bf7479dd54fd44ee283d97
nathaniel simon <nathanielkatesimon@gmail.com>
2023-01-22 08:38:09 +0800

    created a form for results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,8 +3,12 @@ class FlightsController < ApplicationController
     def index
         from = Airport.find_by(name: get_params[:from])
         to = Airport.find_by(name: get_params[:to])
-
+        @passengers = get_params[:passengers]
         @results = Flight.where("from_id = ? or to_id = ?", from.id, to.id)
+    end
+
+    def create
+        fail
     end
 
     private
